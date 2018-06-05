78cdc3c711951104a9055de9e97ff1efd71fa484
Nikita Lobachev <sanniassin@gmail.com>
2018-06-05 20:25:31 +0300

    Fetch and render Contentful entries

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
+
+import { loadEntries } from 'ducks/content';
 
 import './style/style.scss';
 
 
 const mapStateToProps = (state) => {
-  return {};
+  return {
+    entries: state.content.entries.items
+  };
 };
 
 class App extends React.Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+
+    dispatch(loadEntries());
+  }
+
   render() {
+    const { entries } = this.props;
+
     return (
-      <div className="main">ESPI Test</div>
+      <div className="main">
+        { entries ?
+            entries.map(entry => (
+              <div key={entry.sys.id}>
+                <ReactMarkdown
+                  source={
+                    entry.fields.modules
+                    ? entry.fields.modules[0].fields.copy
+                    : entry.fields.copy
+                  }
+                />
+              </div>
+            ))
+          :
+           'Loading'
+        }
+      </div>
     );
   }
 }
--- src/ducks/content.js
+++ src/ducks/content.js
@@ -1,8 +1,45 @@
-const initialState = {};
+import immutableUpdate from 'immutability-helper';
+import * as contentful from 'contentful';
+
+export const CONTENT_LOAD_ENTRIES_SUCCESS = 'CONTENT_LOAD_ENTRIES_SUCCESS';
+
+const client = contentful.createClient({
+  space: 'ykzoqp0e2iu9',
+  accessToken: '7bb08ae353b68f2b12bdf8ac8984e9a1cab50d7401bdd318c558000f106aaab4'
+});
+
+const initialState = {
+  entries: {}
+};
+
+export const fetchEntries = () => {
+  return client.getEntries();
+};
+
+const loadEntriesSuccess = (payload) => {
+  return {
+    type: CONTENT_LOAD_ENTRIES_SUCCESS,
+    payload
+  };
+};
+
+export const loadEntries = () => {
+  return (dispatch) => {
+    return fetchEntries()
+      .then((payload) => {
+        dispatch(loadEntriesSuccess(payload));
+        return payload;
+      });
+  };
+};
 
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
+    case CONTENT_LOAD_ENTRIES_SUCCESS:
+      return immutableUpdate(state, {
+        entries: { $set: action.payload }
+      });
     default:
       return state;
   }
