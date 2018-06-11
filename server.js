779d24655584642b1a259a97ab658a42787badb2
Nikita Lobachev <sanniassin@gmail.com>
2018-06-11 15:15:30 +0300

    Serve storybooks at /static

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const publicPath = path.resolve(__dirname, 'www');
+const storybookPath = path.resolve(__dirname, 'storybook-static');
 
 app.use(express.static(publicPath));
+app.use('/static', express.static(storybookPath));
 app.use((req, res) => {
   res.status(200).sendFile('index.html', { root: publicPath });
 });
